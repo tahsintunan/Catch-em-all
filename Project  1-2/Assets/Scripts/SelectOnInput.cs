1b84d34fd14512274aa804244a4c82da238a9ddd
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2019-10-28 11:24:00 +0600

    Update Game Level

---
+++ Project  1-2/Assets/Scripts/SelectOnInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour
+{
+    public EventSystem eventSystem;
+    public GameObject selectedObject;
+
+    private bool buttonSelected;
+
+        void Start()
+        {
+        
+        }
+
+        void Update()
+        {
+            if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false)
+            {
+                eventSystem.SetSelectedGameObject (selectedObject);
+                buttonSelected = true;
+            }
+        }
+
+        private void OnDisable()
+        {
+            buttonSelected = false; 
+        }
+}
