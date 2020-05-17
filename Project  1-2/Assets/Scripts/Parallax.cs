6423429828759a4dfb4c6dd01f015cde6b96858d
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2020-05-18 00:11:52 +0600

    Build 2.0
    Failed to get a parallax effect. Fixed the pause menu.

---
+++ Project  1-2/Assets/Scripts/Parallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float length, startpos;
+    public GameObject cam;
+    public float parallexEffect;
+    void Start()
+    {
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
+    }
+}
